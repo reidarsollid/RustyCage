d2b27fbfc1504513c60b6500533c9e3e4c5ecbae
Reidar Sollid <rsollid@gmail.com>
2012-03-23 01:12:14 +0100

    Forgotten refactored wizards

---
+++ src/org/baksia/rustycage/wizards/RustProjectPage.java
@@ -0,0 +1,109 @@
+package org.baksia.rustycage.wizards;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IProjectDescription;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ModifyEvent;
+import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+
+public class RustProjectPage extends WizardPage {
+    private Text projectText;
+
+    private IProject project;
+
+    private ISelection selection;
+
+    protected RustProjectPage(ISelection selection) {
+        super("rustProjectWizard");
+        setTitle("Rust project wizard");
+        setDescription("Set Rust project name");
+        this.selection = selection;
+    }
+
+    @Override
+    public void createControl(Composite parent) {
+        Composite container = new Composite(parent, SWT.NULL);
+        GridLayout layout = new GridLayout();
+        container.setLayout(layout);
+        layout.numColumns = 2;
+        layout.verticalSpacing = 9;
+        Label label = new Label(container, SWT.NULL);
+        label.setText("&Project name:");
+
+        projectText = new Text(container, SWT.BORDER | SWT.SINGLE);
+        GridData gd = new GridData(GridData.FILL_HORIZONTAL);
+        projectText.setLayoutData(gd);
+        projectText.addModifyListener(new ModifyListener() {
+            @Override
+            public void modifyText(ModifyEvent e) {
+                dialogChanged();
+            }
+        });
+
+        initialize();
+        dialogChanged();
+        setControl(container);
+    }
+
+    private void initialize() {
+        projectText.setText("RustProject");
+    }
+
+    public boolean createProject() {
+        if (project != null) {                                                    
+            IProjectDescription desc = project.getWorkspace().newProjectDescription(project.getName());
+            try {
+                project.create(desc, null);
+                if (!project.isOpen()) {
+                    project.open(null);
+                }
+                IFolder src = project.getFolder("src");
+                if (!src.exists()) {
+                    src.create(false, true, null);
+                }
+                IFolder out = project.getFolder("bin");
+                if (!out.exists()) {
+                    out.create(false, true, null);
+                }
+            } catch (CoreException e) {
+                updateStatus(e.getMessage());
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private void dialogChanged() {
+        if(getProjectName().isEmpty()){
+            return;
+        }
+        project = ResourcesPlugin.getWorkspace().getRoot()
+                .getProject(getProjectName());
+
+        if (project.exists()) {
+            updateStatus("Project already exists");
+            return;
+        }
+        updateStatus(null);
+    }
+
+    private void updateStatus(String message) {
+        setErrorMessage(message);
+        setPageComplete(message == null);
+    }
+
+    public String getProjectName() {
+        return projectText.getText();
+    }
+
+}
---
+++ src/org/baksia/rustycage/wizards/RustProjectWizard.java
@@ -0,0 +1,35 @@
+package org.baksia.rustycage.wizards;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.Wizard;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+
+public class RustProjectWizard extends Wizard implements INewWizard {
+
+    private RustProjectPage rustProjectPage;
+    private ISelection selection;
+
+
+    public RustProjectWizard() {
+        setNeedsProgressMonitor(true);
+    }
+
+    @Override
+    public void addPages() {
+        rustProjectPage = new RustProjectPage(selection);
+        addPage(rustProjectPage);
+    }
+
+    @Override
+    public void init(IWorkbench workbench, IStructuredSelection selection) {
+
+    }
+
+    @Override
+    public boolean performFinish() {
+        return rustProjectPage.createProject();
+    }
+
+}
