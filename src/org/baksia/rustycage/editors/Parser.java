c909cec7bd780aadf6e01fec5c3aa27b2c8adb5a
Reidar Sollid <rsollid@gmail.com>
2013-04-07 21:51:52 +0200

    Coins

--- src/org/baksia/rustycage/editors/Parser.java
+++ src/org/baksia/rustycage/editors/Parser.java
@@ -9,7 +9,7 @@ public class Parser {
             "if", "iface", "impl", "import", "in", "int", "let", "log", "mod", "mut",
             "native", "note", "prove", "pure", "resource", "return", "self", "str", "syntax",
             "tag", "true", "type", "u16", "u32", "u64", "u8", "uint", "unchecked", "unsafe",
-            "use", "vec", "while", "enum", "copy", "loop", "match"};
+            "use", "vec", "while", "enum", "copy", "loop", "match", "move"};
 
     public static final String[] NEW_FILE = {"import", "export", "use", "mod", "dir"};
 }
