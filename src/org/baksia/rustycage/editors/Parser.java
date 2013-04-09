7c7f27fd3da82bb1050d3cc1dbee62d048118e0b
Reidar Sollid <rsollid@gmail.com>
2013-04-09 23:30:42 +0200

    0.6 keywords and replaced deprecated extension point for contentTypes

--- src/org/baksia/rustycage/editors/Parser.java
+++ src/org/baksia/rustycage/editors/Parser.java
@@ -2,14 +2,21 @@ package org.baksia.rustycage.editors;
 
 public class Parser {
     public static final String[] KEYWORDS = {
-            "alt", "any", "as", "assert", "be", "bind", "block", "bool", "break",
-            "char", "check", "claim", "const", "cont", "class",
-            "do", "else", "export",
-            "f32", "f64", "fail", "false", "float", "fn", "for", "i16", "i32", "i64", "i8",
-            "if", "iface", "impl", "import", "in", "int", "let", "log", "mod", "mut",
-            "native", "note", "prove", "pure", "resource", "return", "self", "str", "syntax",
-            "tag", "true", "type", "u16", "u32", "u64", "u8", "uint", "unchecked", "unsafe",
-            "use", "vec", "while", "enum", "copy", "loop", "match", "move"};
+            "as",
+            "break",
+            "copy",
+            "do", "drop",
+            "else", "enum", "extern",
+            "false", "fn", "for",
+            "if", "impl",
+            "let", "loop",
+            "match", "mod", "mut",
+            "priv", "pub",
+            "ref", "return",
+            "self", "static", "struct", "super",
+            "true", "trait", "type",
+            "unsafe", "use",
+            "while"};
 
-    public static final String[] NEW_FILE = {"import", "export", "use", "mod", "dir"};
+    public static final String[] NEW_FILE = {"extern", "use", "mod"};
 }
