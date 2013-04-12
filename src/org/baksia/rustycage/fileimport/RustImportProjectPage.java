3c179e561f4059cf4b2eb22cc1868ac9b31e73cd
Reidar Sollid <rsollid@gmail.com>
2013-04-12 20:27:08 +0200

    Added file import for Rust files

---
+++ src/org/baksia/rustycage/fileimport/RustImportProjectPage.java
@@ -0,0 +1,10 @@
+package org.baksia.rustycage.fileimport;
+
+import org.eclipse.ui.dialogs.WizardNewProjectCreationPage;
+
+public class RustImportProjectPage extends WizardNewProjectCreationPage {
+
+    public RustImportProjectPage() {
+        super("RustImportProjectWizard");
+    }
+}
