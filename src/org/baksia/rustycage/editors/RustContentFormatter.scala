1aa6fe6d2566dbceca6b7a79b4a17deecd629c12
Reidar Sollid <rsollid@gmail.com>
2013-10-17 08:06:19 +0200

    Indention formatting

--- src/org/baksia/rustycage/editors/RustContentFormatter.scala
+++ src/org/baksia/rustycage/editors/RustContentFormatter.scala
@@ -4,8 +4,9 @@ import org.eclipse.jface.text.formatter.{MultiPassContentFormatter, IFormattingS
 import org.eclipse.jface.text.IDocument
 import org.eclipse.jface.text.IRegion
 import org.eclipse.jface.text.source.ISourceViewer
+import org.eclipse.jface.text.formatter.ContextBasedFormattingStrategy
 
-class RustContentFormatter(val sourceView: ISourceViewer) extends IContentFormatter {
+class RustContentFormatter(val sourceView: ISourceViewer) extends ContextBasedFormattingStrategy {
 	def format( document: IDocument,  region: IRegion) {}
 	
 	def getFormattingStrategy(contentType: String):IFormattingStrategy = null
