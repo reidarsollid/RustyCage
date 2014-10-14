45b466cd6fff261a1f7c06f6668a50248916d8a4
Anttoni <itsdarkinside@gmail.com>
2014-10-14 18:08:48 +0300

    Fixed runner, should work now

--- rustycage-plugin/src/org/rustycage/run/RustRunner.scala
+++ rustycage-plugin/src/org/rustycage/run/RustRunner.scala
@@ -1,16 +1,16 @@
 package org.rustycage.run
 
-import org.eclipse.core.commands.{ExecutionEvent, AbstractHandler}
-import org.eclipse.ui.handlers.HandlerUtil
-import org.eclipse.ui.texteditor.ITextEditor
-import org.eclipse.core.resources.{IFile, IContainer, IProject}
-import org.rustycage.editors.RustEditor
-import org.eclipse.jface.preference.IPreferenceStore
-import org.rustycage.RustPlugin
-import org.eclipse.ui.internal.Workbench
-import org.eclipse.jface.viewers.IStructuredSelection
+import org.eclipse.core.commands.AbstractHandler
+import org.eclipse.core.commands.ExecutionEvent
+import org.eclipse.core.resources.IFile
+import org.eclipse.core.resources.IFolder
+import org.eclipse.core.resources.IProject
 import org.eclipse.core.resources.IResource
-import org.eclipse.jface.text.ITextSelection
+import org.eclipse.jface.preference.IPreferenceStore
+import org.eclipse.jface.viewers.IStructuredSelection
+import org.eclipse.ui.internal.Workbench
+import org.rustycage.RustPlugin
+import org.rustycage.editors.RustEditor
 
 
 class RustRunner extends AbstractHandler {
@@ -23,14 +23,13 @@ class RustRunner extends AbstractHandler {
 
   import scala.sys.process._
   def run(project: IProject) {
-    val bin = project.getFolder("bin")
-    val messageConsole = new MessageConsoleScala(findExecutable(bin, project), "Run : ")
-    messageConsole.message("Running: " + findExecutable(bin, project))
+    val messageConsole = new MessageConsoleScala(findExecutable.asInstanceOf[IFile], "Run : ")
+    messageConsole.message("Running: " + findExecutable)
     val logger = ProcessLogger(
       (o: String) => messageConsole.message(o),
       (e: String) => messageConsole.errorMessage(e))
 
-    findExecutable(bin, project).getLocationURI().getRawPath() ! logger
+    findExecutable ! logger
     messageConsole.close()
 
   }
@@ -53,18 +52,32 @@ class RustRunner extends AbstractHandler {
     null
   }
   
-  def findExecutable(dir: IContainer, project: IProject): IFile = {
+  import scala.util.control.Breaks._
+  def findExecutable(): String = {
+    var outDir: String = ""
+    
     val prefStore: IPreferenceStore = RustPlugin.prefStore
+    val runtime = prefStore.getString("RUNTIME_ARGS").split(" ")
     
-    if(!prefStore.getString("RUNTIME_ARGS").contains("--out-dir")) {
-      val projectFiles = dir.members().filter(r => r.getFileExtension == ".exe")
-      if(projectFiles(0) != null) projectFiles(0)
+    for(i <- 0 to runtime.length) {
+      if(runtime(i) == "--output-dir") {
+        outDir = runtime(i + 1)
+        break;
+      }
     }
     
-    val files = dir.members().filter(r => r.getFileExtension == ".exe")
-    if(files(0) != null) {
-      files(0)
+    if(!outDir.isEmpty()) {
+      val executable = getActiveProject.getFolder(outDir).members()
+                                       .filter(r => r.getFileExtension() == ".exe")
+      if(executable.length > 0)
+        executable(0)
     }
+    
+    val root = getActiveProject.members().filter(r => r.getFileExtension() == ".exe")
+    
+    if(root.length > 0)
+      root(0)
+    
     throw new RuntimeException("Bin folder should only consist of one executable")
   }
   
